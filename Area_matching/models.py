26782f5c0603945796d597d97ae5aed3a4deb8d7
Bishwas Koirala <wasubisu69@gmail.com>
2024-10-19 18:20:47 +0900

    backend : model created

--- Area_matching/models.py
+++ Area_matching/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Area(models.Model) :
+    area_name = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.area_name
+
+
+class Hobby(models.Model):
+    hobby_name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.hobby_name
+
+
+class User(models.Model):
+    name = models.CharField(max_length=20) 
+    area = models.ForeignKey(Area, on_delete=models.SET_NULL,null=True,related_name='user')
+    hobby = models.ManyToManyField(Hobby)
+
+    def __str__(self):
+        return self.name
+    
+class Group(models.Model) : 
+    group_name = models.CharField(max_length=20)
+    admin_user = models.ForeignKey(User, on_delete=models.SET_NULL , null=True , related_name='group')
+    def __str__(self):
+        return self.group_name
+    
+
+
+
+class Chat(models.Model) :
+    group_id = models.ForeignKey(Group, on_delete=models.CASCADE , related_name='chat')
+    user_id = models.ForeignKey(User,on_delete=models.CASCADE, related_name='user')
+    text = models.TextField(default=None)
+    posted_at=models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.chat_name
